837eee4d612a426eb9ca139da3c1e7873d9905a0
Stanimir Georgiev <s.georgiev@fio.de>
2020-12-01 16:32:13 +0200

    Add react router with header

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { rootComponents } from './config/app-factory';
-// import { Tasks } from './tasks';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Header } from './controls/header/header';
+
+import './app.css';
 
 export const App = () => {
     return (
         <>
-            {rootComponents.map((Component, index) => {
-                return <Component key={index} />;
-            })}
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        {rootComponents.map((Component, index) => {
+                            return <Component key={index} />;
+                        })}
+                    </Route>
+                    <Route path="/about">About</Route>
+                </Switch>
+            </Router>
         </>
     );
 };
---
+++ src/controls/header/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './header.css';
+
+export const Header = () => {
+    let history = useHistory();
+    return (
+        <>
+            <div className="header">
+                <div className="header-container">
+                    <ul className="header-item-wrapper">
+                        <li className="header-item" onClick={() => history.push('/')}>
+                            Home
+                        </li>
+                        <li className="header-item" onClick={() => history.push('/about')}>
+                            About
+                        </li>
+                    </ul>
+                    <hr />
+                </div>
+            </div>
+        </>
+    );
+};
